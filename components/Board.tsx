c35a741f07cc13d2e63e567901e6f2182eed2805
fourhtyoz <fourhtyoz@gmail.com>
2024-04-21 00:51:04 +0300

    Column

--- components/Board.tsx
+++ components/Board.tsx
@@ -2,7 +2,8 @@
 
 import { useBoardStore } from '@/store/BoardStore';
 import React, { useEffect } from 'react'
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
+import Column from './Column';
 
 
 function Board() {
@@ -12,23 +13,32 @@ function Board() {
         getBoard()
     }, [getBoard])
 
+    const handleOnDragEnd = (result: DropResult) => {
+
+    }
+
     return (
-        // <>hi</>
-            <DragDropContext>
-                <Droppable 
-                    droppableId='board' 
-                    direction='horizontal'
-                    type='column'
-                >
-                    {provided => (
-                        <div>
-                            <div className=''>
-                                
-                            </div>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+            <Droppable 
+                droppableId='board' 
+                direction='horizontal'
+                type='column'
+            >
+                {provided => (
+                    <div 
+                        className='grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto'
+                        {...provided.droppableProps} 
+                        ref={provided.innerRef}>
+                        <div>{
+                            Array.from(board.columns.entries()).map(([id, column], index) => (
+                                <Column key={id} id={id} todos={column.todos} index={index} />
+                            ))
+                        }
                         </div>
-                    )}
-                </Droppable>
-            </DragDropContext>
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
     )
 
 }
