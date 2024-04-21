ca7a822c134cdfc74028d8851dbd62165511112f
fourhtyoz <fourhtyoz@gmail.com>
2024-04-21 13:10:39 +0300

    dnd

--- components/Board.tsx
+++ components/Board.tsx
@@ -25,6 +25,59 @@ function Board() {
             const rearragnedColumns = new Map(entries)
             setBoardState({...board, columns: rearragnedColumns})
         }
+
+        const columns = Array.from(board.columns)
+        const startColIndex = columns[Number(source.droppableId)]
+        const finishColIndex = columns[Number(destination.droppableId)]
+
+        const startCol: Column = {
+            id: startColIndex[0],
+            todos: startColIndex[1].todos
+        }
+
+        const finishCol: Column = {
+            id: finishColIndex[0],
+            todos: finishColIndex[1].todos
+        }
+
+        if (!startCol || !finishCol) return;
+        if (source.index === destination.index && startCol === finishCol) return
+
+        const newTodos = startCol.todos
+        const [todoMoved] = newTodos.splice(source.index, 1)
+
+        if (startCol.id === finishCol.id) {
+            newTodos.splice(destination.index, 0, todoMoved)
+            const newCol = {
+                id: startCol.id,
+                todos: newTodos
+            }
+            const newColumns = new Map(board.columns);
+            newColumns.set(startCol.id, newCol)
+
+            setBoardState({...board, columns: newColumns})
+        } else {
+            const finishTodos = Array.from(finishCol.todos)
+            finishTodos.splice(destination.index, 0, todoMoved);
+
+            const newColumns = new Map(board.columns)
+            const newCol = {
+                id: startCol.id,
+                todos: newTodos
+            }
+
+            newColumns.set(startCol.id, newCol)
+            newColumns.set(finishCol.id, {
+                id: finishCol.id,
+                todos: finishTodos
+            })
+
+            setBoardState({...board, columns: newColumns})
+
+
+
+
+        }
     }
 
     return (
